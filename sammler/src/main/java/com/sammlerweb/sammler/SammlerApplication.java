d8b36b81a0e085071c7b202a7a2038a9d8467647
mbp987 <31522029+mbp987@users.noreply.github.com>
2021-12-17 13:44:18 +0100

    Add Logback
    logs to FILE and STDOUT

--- sammler/src/main/java/com/sammlerweb/sammler/SammlerApplication.java
+++ sammler/src/main/java/com/sammlerweb/sammler/SammlerApplication.java
@@ -1,13 +1,19 @@
 package com.sammlerweb.sammler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SammlerApplication {
 
+	private static final Logger LOGGER=LoggerFactory.getLogger(SammlerApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SammlerApplication.class, args);
+		
+		LOGGER.info("Log from main");
 	}
 
 }
