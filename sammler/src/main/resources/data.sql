6e9332c75bd1f1fb5aa8db74e09c99071ca778a0
mbp987 <31522029+mbp987@users.noreply.github.com>
2022-01-08 21:52:00 +0100

    H2 database corrections

--- sammler/src/main/resources/data.sql
+++ sammler/src/main/resources/data.sql
@@ -1,8 +1,4 @@
-DROP TABLE IF EXISTS banknotes;
-
-CREATE TABLE banknotes (
-id INTEGER PRIMARY KEY,
-value INTEGER NOT NULL,
-serial_number VARCHAR(30);
-
-INSERT INTO banknotes (id, value, serial_number) VALUES (0001, 50, ‘AB12345’);
+INSERT INTO BANKNOTES (obverse_value, serial_number) VALUES
+  ('50', 'ABC123'),
+  ('10', 'XYZ987'),
+  ('200', 'XXX555');
