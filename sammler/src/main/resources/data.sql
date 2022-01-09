b6e8f51271e621e9abb7c7c2cf8b063f3eb9c176
mbp987 <31522029+mbp987@users.noreply.github.com>
2022-01-09 21:32:07 +0100

    Create table for banknotes

--- sammler/src/main/resources/data.sql
+++ sammler/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO BANKNOTES (obverse_value, serial_number) VALUES
-  ('50', 'ABC123'),
-  ('10', 'XYZ987'),
-  ('200', 'XXX555');
+INSERT INTO BANKNOTES (country, currency, valueb, issue_year, issue_institution, city, serial_number, conditionb, error, details, other) VALUES
+  ('Germany','German mark' ,50, 1960, 'Deutsche Bundesbank', 'Frankfurt am Main', 'ABC123', 'good', 0, 'abc', 'xyz'),
+  ('Germany','German mark' ,100, 1920, 'Reichsbankdirektorium', 'Berlin', 'XYZ987', 'very good', 1, 'abc', 'xyz'),
+  ('Portugal','escudo' ,500, 1980, 'Banco de Portugal', 'Lisboa', 'PPP55555', 'very good', 0, 'abc', 'xyz');
--- sammler/src/main/resources/schema.sql
+++ sammler/src/main/resources/schema.sql
@@ -1,7 +1,14 @@
-DROP TABLE IF EXISTS BANKNOTES;
-
 CREATE TABLE BANKNOTES (
 id INT AUTO_INCREMENT PRIMARY KEY,
-obverse_value VARCHAR(16) NOT NULL,
-serial_number VARCHAR(30) NOT NULL
+country VARCHAR(40) NOT NULL,
+currency VARCHAR(25),
+valueb INT NOT NULL,
+issue_year INT,
+issue_institution VARCHAR(30),
+city VARCHAR(20),    
+serial_number VARCHAR(30) NOT NULL,
+conditionb VARCHAR(10),
+error BOOLEAN,
+details VARCHAR(50),
+other VARCHAR(5)
 );
